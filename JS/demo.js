9b1b95ba35be15386bd8d424d7b1fa9417ec5a77
Kuanlin-Chen <webb1992810625@gmail.com>
2017-06-07 10:49:54 +0800

    [New Folder]Add some practice of Javascript into this project.

---
+++ JS/demo.js
@@ -0,0 +1,6 @@
+document.write("Hello, World!");
+var word = "\nMy name";
+var name = " is KuanlinChen";
+var sentence = word + name;
+document.write(sentence);
+alert("Hello, This is a demo for javascript!");
